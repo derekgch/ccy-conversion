38aab4250d82d3948931f9af79bf12f66622505d
Derek G Chen <derek@derekgc.com>
2020-08-10 19:08:10 -0400

    spring init

---
+++ src/test/java/com/conversion/ccyconversion/CcyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversion.ccyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
